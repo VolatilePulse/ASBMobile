a1993fbe505c39fb1dd1554e1bfb8add9bfd08d1
VolatilePulse <shearer.zachary@gmail.com>
2018-02-24 19:54:53 -0500

    Add DB.bulkPut

--- src/Data.js
+++ src/Data.js
@@ -35,14 +35,16 @@ var Data = {
             app.officialServerSettings = new ASBM.Server(jsonObject.statMultipliers, null, false, jsonObject.imprintingMultiplier);
             app.officialSPSettings = new ASBM.Server(jsonObject.statMultipliersSP, app.officialServerSettings, true, jsonObject.imprintingMultiplier);
             
+            // Array to pass the multipliers using .bulkPut
+            var linearArray = [];
+
             // Create the DB
-            for (var species in app.myMultipliers) {
-               for (var index = 0; index < 8; index ++) {
-                  let putPromise = app.multipliersDB.statMultiplier
-                     .put(Object.assign({}, app.myMultipliers[species][index], {species: species, statIndex: index}));
-                  promises.push(putPromise);
-               }
-            }
+            for (var species in app.myMultipliers)
+               for (var index = 0; index < 8; index ++)
+                  linearArray.push(Object.assign({}, app.myMultipliers[species][index], {species: species, statIndex: index}));
+
+            let putPromise = app.multipliersDB.statMultiplier.bulkPut(linearArray);
+            promises.push(putPromise);
          }
 
          // Otherwise the DB already exists
@@ -58,59 +60,5 @@ var Data = {
          }
 
          Promise.all(promises).then(() => resolve()).catch(err => reject(err));
-   })},
-
-   // Attempt to make LoadValues a promise instead of a function
-   // resolve is an expected good return
-   // Whatever is passed to resolve, is passed on to the next .then call
-   // reject if a failed result
-   // reject is sent to .catch
-   // or both can be processed by (function(param1, param2) {}) where param1 is the content of a success event
-   // and param2 is the content of a failed event
-   // Expected usage:
-   // GetFile: Load asset from cache/server
-   // ObjectCreation: Convert the json into ASBM Objects
-   // SetVueObjects: Assign necessary variables to Vue elements
-   // CriticalError: Suggest user refreshes their browser or goes online
-   //    GetFile("file.ext").then(ObjectCreation).then(SetVueObjects).catch(CriticalError);
-
-   // We will only get a json string if the file loaded correctly
-   // Convert Cad's object to ASBM objects
-   LoadValues2 (json) {
-      return new Promise(function(resolve, reject) {
-         app.multipliersDB.version(DB_VER_M).stores({
-            multiplier: 'B, Id, Iw, TBHM, Ta, Tm, IBM'
-         });
-         // Check to see if the json file has been updated
-         // if (json === "") {
-         if (true) {
-            // File hasn't been updated continue using the DB
-         }
-         
-         // We want to either resolve to the next function, or just return to it in general
-         // We want the next function to handle the assignment of variables to Vue
-         var jsonObject = JSON.parse(json);
-         for(var i in jsonObject.species) {
-            app.myMultipliers[jsonObject.species[i].name] = new ASBM.StatMultipliers(
-               jsonObject.species[i].statsRaw,
-               jsonObject.species[i].TBHM,
-               jsonObject.species[i].doesNotUseOxygen,
-               jsonObject.species[i].NoImprintingForSpeed);
-         }
-
-         console.log(app.myMultipliers[Argentavis][0]);
-
-         resolve(true)
-
-         /*for (var species in app.myMultipliers)
-            for (var i in app.myMultipliers[species])
-               */
-
-         app.officialServerSettings = new ASBM.Server(jsonObject.statMultipliers, null, false, jsonObject.imprintingMultiplier);
-         app.officialSPSettings = new ASBM.Server(jsonObject.statMultipliersSP, app.officialServerSettings, true, jsonObject.imprintingMultiplier);
-
-         // Allows next .then to access myMultipliers directly once we are done with it
-         resolve(app.myMultipliers);
-      });
-   }
+   })}
 }
