6fb2eb5665f1b13cb6122750aede8425d2296bff
Ant Purchase <antoranthony.purchase@gmail.com>
2018-04-04 01:47:55 +0100

    Add homemade TypeScript definitions for pouchdb-live-find

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -6,3 +6,40 @@ declare module 'bootstrap-vue';
 declare module 'pouch-vue';
 declare module 'pouchdb-live-find';
 
+// Home-made type completion for pouchdb-live-find
+declare namespace PouchDB {
+   namespace LiveFind {
+      interface CreateOptions<Content extends {}> extends PouchDB.Find.FindRequest<Content> {
+         aggregate?: boolean;
+      }
+
+      interface PaginateOptions {
+         sort?: Array<string | { [propName: string]: 'asc' | 'desc' }>;
+         skip?: number;
+         limit?: number;
+      }
+
+      interface Change<Content extends {}> {
+         action: 'ADD' | 'UPDATE' | 'REMOVE';
+         id: string;
+         rev: string;
+         doc: Content;
+      }
+
+      interface Controller<Content extends {}> {
+         on(event: 'update', fn: (update: Change<Content>, aggregate: Content[]) => void): void;
+         on(event: 'ready' | 'cancelled', fn: () => void): void;
+         on(event: 'error', fn: (err: Error) => void): void;
+
+         cancel(): void;
+         then<T>(fn: (result: { docs: Content[] }) => any | Promise<T>): Promise<T>;
+         //catch():Promise<void>;
+         sort(sort: Array<string | { [propName: string]: 'asc' | 'desc' }>): void;
+         paginate(options: PaginateOptions): void;
+      }
+   }
+
+   interface Database<Content extends {}={}> {
+      liveFind(options: LiveFind.CreateOptions<Content>): LiveFind.Controller<Content>;
+   }
+}
