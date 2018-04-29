98e4bbc397465253a01341f29e2a52b75815defd
Ant Purchase <antoranthony.purchase@gmail.com>
2018-04-29 13:53:44 +0100

    Utilise all test data in command-line tests
    The pass conditions for these tests are different to to the Tester tab. Currently it considers at least one valid option set as a pass.

--- tests/unit/extractor.spec.ts
+++ tests/unit/extractor.spec.ts
@@ -1,10 +1,20 @@
 import { ParseDatabase } from '@/ark/data';
-import testData from '@/ark/test_data';
+import { TestData } from '@/ark/types';
 import * as Servers from '@/servers';
 import { PerformTest } from '@/testing';
 import theStore from '@/ui/store';
 import { expect } from 'chai';
 import { readFileSync } from 'fs';
+
+
+const L1_REX: TestData = {
+   tag: 'TE 100%',
+   species: 'Rex', level: 1, imprint: 0, mode: 'Tamed',
+   values: [1100.1, 420, 150, 3000, 500, 125.8, 100, 1550.5],
+   serverId: 'predef:Official Server',
+   // tslint:disable-next-line:max-line-length
+   results: [[{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }], [{ Lw: 0, Ld: 0 }]],
+};
 
 
 before('load values', () => {
@@ -25,9 +35,7 @@ describe('values.json', () => {
 
 describe('extractor', () => {
    it('should extract L1 Rex in official', () => {
-      const data = testData[0];
-      const result = PerformTest(data);
-
+      const result = PerformTest(L1_REX);
       expect(result).to.have.property('pass').which.equals(true);
       expect(result.options).to.have.length.gt(0);
    });
---
+++ tests/unit/test_data.spec.ts
@@ -0,0 +1,22 @@
+import { ParseDatabase } from '@/ark/data';
+import testData from '@/ark/test_data';
+import * as Servers from '@/servers';
+import { PerformTest } from '@/testing';
+import { expect } from 'chai';
+import { readFileSync } from 'fs';
+
+
+before('load values', () => {
+   const valuesJson = readFileSync('public/data/data.json').toString();
+   ParseDatabase(valuesJson);
+   Servers.initialise();
+});
+
+describe('test_data', () => {
+   testData.forEach((data, i) =>
+      it(`should extract L${data.level} ${data.mode.toLowerCase()} ${data.species} [index ${i}]`, () => {
+         const result = PerformTest(data);
+         // expect(result, 'contains .pass').to.have.property('pass').which.equals(true);
+         expect(result.options, 'has at least one option').to.have.length.gt(0);
+      }));
+});
