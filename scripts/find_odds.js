cf401ee3b26bbb0cea627b612d0a4bb6663a4ed5
Ant Purchase <antoranthony.purchase@gmail.com>
2018-05-16 22:34:43 +0100

    Minor update to script

--- scripts/find_odds.js
+++ scripts/find_odds.js
@@ -1,5 +1,5 @@
-import Enumerable from 'linq';
-import * as fs from 'fs';
+const Enumerable = require('linq');
+const fs = require('fs');
 
 const DATA = "public/data/data.json";
 const STAT_B = 0, STAT_IW = 1, STAT_ID = 2, STAT_TA = 3, STAT_TM = 4;
@@ -18,21 +18,21 @@ function readData() {
  * Your filter logic here...
  * You're passed the species stat values.
  * Return true if you want to include this species in the results.
- * @param {number[][]} stats
+ * @param {any} species
  * @returns {boolean} True if this species should be selected.
  */
-function myFilter(stats) {
-   return Enumerable.from(stats).any(values => Enumerable.from(values.slice(STAT_TM)).all(v => v < 0));
+function myFilter(species) {
+   return (species.TBHM != undefined && species.TBHM != 1); // || Enumerable.from(species.stats).any(values => values[STAT_TA] < 0);
 }
 
 let values = readData();
 
 let matchingSpecies = Enumerable.from(values.species).
-   where(kvp => myFilter(kvp.value.stats)).
+   where(kvp => myFilter(kvp.value)).
    select(kvp => Object.assign({}, kvp.value, { species: kvp.key })).
    toArray();
 
 for (var data of matchingSpecies) {
-   console.log(`${data.species}...`);
-   console.dir(data.stats);
+   console.log(`${data.species}...   (TBHM=${data.TBHM || '-'})`);
+   // console.dir(data.stats);
 }
