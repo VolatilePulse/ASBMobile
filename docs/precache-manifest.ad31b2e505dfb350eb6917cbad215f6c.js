d383fdd9e4df80d2d46a5f990124cdf6f63b652e
VolatilePulse <shearer.zachary@gmail.com>
2018-06-09 18:00:23 -0400

    Release Build

---
+++ docs/precache-manifest.ad31b2e505dfb350eb6917cbad215f6c.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3f2d64def920ce2d82cf6101a7b496e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7770c509f154f506cd1df0ab8dc36ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "917b183d8a0b7899f71b98394bee1d19",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "c5b66772d975bd7edf9e",
+    "url": "/css/chunk-vendors.c91fa4d7.css"
+  },
+  {
+    "revision": "ca81ccf0830e4fec0d5b12e765fe4df1",
+    "url": "/img/asbm.ca81ccf0.svg"
+  },
+  {
+    "revision": "a38cad462cf441d091bc97c2685212ec",
+    "url": "/img/hand.a38cad46.svg"
+  },
+  {
+    "revision": "b8a6b3702a1f938797a2b4d0f0235050",
+    "url": "/img/asbm-inline.b8a6b370.svg"
+  },
+  {
+    "revision": "c9a690572a0e6520c994078696f9e34b",
+    "url": "/img/delete.c9a69057.svg"
+  },
+  {
+    "revision": "7aafa3ef6b4a0320733fe111cfc9551e",
+    "url": "/img/melee.7aafa3ef.svg"
+  },
+  {
+    "revision": "2baa77d6015e6de6adf1ffa247530568",
+    "url": "/img/webpack.2baa77d6.svg"
+  },
+  {
+    "revision": "5045194579b1d155f3506513ef0dd716",
+    "url": "/img/face.50451945.svg"
+  },
+  {
+    "revision": "69fce054d6d7d535c87245193b34c6a9",
+    "url": "/img/vue.69fce054.svg"
+  },
+  {
+    "revision": "e37677b454382e8137cd8e3b2a821b8d",
+    "url": "/img/health.e37677b4.svg"
+  },
+  {
+    "revision": "303f30630ccc131d84525b4852dfc3b7",
+    "url": "/img/weight.303f3063.svg"
+  },
+  {
+    "revision": "58ec3bb74c37f7f92a68d2a6e15f78cd",
+    "url": "/img/github.58ec3bb7.svg"
+  },
+  {
+    "revision": "ff5323839914853e3395863270bcebb2",
+    "url": "/img/oxygen.ff532383.svg"
+  },
+  {
+    "revision": "089d28c9c23ecafd1fbb8257fab72ff0",
+    "url": "/img/pouchdb.089d28c9.svg"
+  },
+  {
+    "revision": "0bfd991719724ab0680dabedd7dbcff6",
+    "url": "/img/play.0bfd9917.svg"
+  },
+  {
+    "revision": "a0f119adfe4fcf1bf75b0a1263ddb366",
+    "url": "/img/typescript.a0f119ad.svg"
+  },
+  {
+    "revision": "4dc3047250ddacafeaf0e6372aa5fd38",
+    "url": "/img/edit.4dc30472.svg"
+  },
+  {
+    "revision": "12608b546cfed5b3c2d7dfc3dbe2c695",
+    "url": "/img/timer.12608b54.svg"
+  },
+  {
+    "revision": "c8726e66027a0e4ec8ef7c64fe483733",
+    "url": "/img/lodash.c8726e66.svg"
+  },
+  {
+    "revision": "8750d11758b5e216e1f089c867f51338",
+    "url": "/img/food.8750d117.svg"
+  },
+  {
+    "revision": "c1ec07a2fa8e2bfbcbca319635dd87a0",
+    "url": "/img/bootstrap.c1ec07a2.svg"
+  },
+  {
+    "revision": "985982b58d53428c4bf47b1ede026bda",
+    "url": "/img/stamina.985982b5.svg"
+  },
+  {
+    "revision": "096818314e119c8bc97bfcc89c838ca2",
+    "url": "/img/torpor.09681831.svg"
+  },
+  {
+    "revision": "835f3b2ce2be7c5015e466009320985a",
+    "url": "/img/speed.835f3b2c.svg"
+  },
+  {
+    "revision": "c5b66772d975bd7edf9e",
+    "url": "/js/chunk-vendors.c5b66772.js"
+  },
+  {
+    "revision": "12a40e1822d3cb6a5527",
+    "url": "/js/app.12a40e18.js"
+  },
+  {
+    "revision": "185d386a657a5e4dd477e8d43e23930e",
+    "url": "/img/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "8f95fb065882d2670200e1b78376e10e",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "588bdc87fd283d2f604a19940c3594a7",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "b39999515bebd8f1803503bf7f23e32b",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "707d7a1de1b3a1f8f67906ed1707a842",
+    "url": "/img/icons/apple-touch-icon-120x120-precomposed.png"
+  },
+  {
+    "revision": "2d3c73570ce001037c194370f2561399",
+    "url": "/data/data.json"
+  },
+  {
+    "revision": "6386a15b082098efbf54aa2860ffa7a9",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "43e2e50d8a008df8bcbff5077af010c0",
+    "url": "/img/icons/apple-touch-icon-152x152-precomposed.png"
+  },
+  {
+    "revision": "ab91453aabdff8c6f3eb09b4adeaa2dd",
+    "url": "/img/icons/apple-touch-icon-180x180-precomposed.png"
+  },
+  {
+    "revision": "7811c518da90d67b8413a24dc518c3c5",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "220f3ac37d6006646a0cf2c178980f4a",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "9816e57ae96c39e52fb887bf5501a0a2",
+    "url": "/img/icons/apple-touch-icon-60x60-precomposed.png"
+  },
+  {
+    "revision": "1998f33a60b566ee8f0a0c6f3267637e",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "cb9a2c4f702e7448b75cb9365633e84f",
+    "url": "/img/icons/apple-touch-icon-76x76-precomposed.png"
+  },
+  {
+    "revision": "7811c518da90d67b8413a24dc518c3c5",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "ab91453aabdff8c6f3eb09b4adeaa2dd",
+    "url": "/img/icons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "bd0bd1be9b03c135500a5df3c9995045",
+    "url": "/img/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "e94ecd045bcbd87d0bea58e2d5175382",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "8f95fb065882d2670200e1b78376e10e",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "894dc4b695ba16ac28318bbdbf72ce54",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "f6dd00ad9a84c8d5592454b1c5f10e7a",
+    "url": "/img/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "053243c0519438f8e1594c76a631a409",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "7d61104677b2bca683cf6609f6a2f741",
+    "url": "/img/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "90a810678158448a9841d14d39af6761",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "12a40e1822d3cb6a5527",
+    "url": "/css/app.50fcca50.css"
+  },
+  {
+    "revision": "8370a9eca5313d23d23c341c9e8114ce",
+    "url": "/browserconfig.xml"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/ASBMobile/precache-manifest.4f591502120c7a225886e48c472522c8.js"
+  "/precache-manifest.ad31b2e505dfb350eb6917cbad215f6c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "asb-mobile"});
