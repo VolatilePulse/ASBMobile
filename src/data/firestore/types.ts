35dac0895af9b62749f23fce9a718e26411379fc
Ant Purchase <antoranthony.purchase@gmail.com>
2018-06-09 02:59:20 +0100

    Change Creature.status to a string

--- src/data/firestore/types.ts
+++ src/data/firestore/types.ts
@@ -3,12 +3,7 @@ export type UserId = string;
 export type LibraryId = string;
 export type ServerId = string;
 
-export enum CreatureStatus {
-   Available = 0,
-   Unavailable = 1,
-   Obelisk = 2,
-   Dead = 3,
-}
+export type CreatureStatus = 'Available' | 'Dead' | 'Obelisk' | 'Unavailable';
 
 export interface Stat {
    Lw: number;
