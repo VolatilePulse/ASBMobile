28bd9c223f6af7019efea2e92b9d4c95357ef615
Ant Purchase <antoranthony.purchase@gmail.com>
2018-06-09 10:49:23 +0100

    Make webpack dev server pick up the dev server API keys

--- src/ui/app_code.ts
+++ src/ui/app_code.ts
@@ -19,20 +19,6 @@ import routerConfig from './router';
  */
 
 
-// Initialize Firebase
-if (firebase.apps.length === 0) {
-   const firebaseConfig = {
-      apiKey: 'AIzaSyDZdHGcb-LUbegNC2v_i40S1MByua0LBEQ', // cspell:disable-line
-      authDomain: 'asbmobile-dev.firebaseapp.com',
-      databaseURL: 'https://asbmobile-dev.firebaseio.com',
-      projectId: 'asbmobile-dev',
-      storageBucket: 'asbmobile-dev.appspot.com',
-      messagingSenderId: '621406128308'
-   };
-   firebase.initializeApp(firebaseConfig);
-}
-
-
 @Component({
    router: routerConfig,
 })
