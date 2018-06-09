3ac668ad84ba60ff09fd59dbf233a0ac8da2535f
Ant Purchase <antoranthony.purchase@gmail.com>
2018-06-09 03:24:09 +0100

    Add placeholder wheel component
    Small fixes to Creature page.

---
+++ src/ui/components/wheel.vue
@@ -0,0 +1,16 @@
+<template>
+   <svg></svg>
+</template>
+
+<style scoped>
+</style>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import Common from '@/ui/common';
+
+@Component({ name: 'Wheel' })
+export default class Wheel extends Common {
+   // ...Vue code here
+}
+</script>
--- src/ui/views/creature.vue
+++ src/ui/views/creature.vue
@@ -1,34 +1,37 @@
 <template>
-   <b-container fluid v-if='cache'>
+   <b-container fluid>
       <h2>Creature</h2>
       <b-alert v-if="cache && cache.error" variant="danger">{{cache.error}}</b-alert>
       <b-btn :to="$route.path+'/edit'" variant="link">edit</b-btn>
-      <div>Name: {{cache.data.name}}</div>
-      <div>Species: {{cache.data.speciesName}}</div>
-      <div>Tribe: {{cache.data.tribe}}</div>
-      <div>{{cache.data.TE}}</div>
-      <div>{{cache.data.WL}}</div>
-      <div>{{cache.data.level}}</div>
-      <div>{{cache.data.isTamed}}</div>
-      <div>{{cache.data.isFemale}}</div>
-      <div>{{cache.data.inputSource}}</div>
-      <div>{{cache.data.father}}</div>
-      <div>{{cache.data.currentServer}}</div>
-      <div>{{cache.data.colors}}</div>
-      <canvas ref="creatureColors" width="150" height="150"></canvas>
-      <div>{{cache.data.breedingValues}}</div>
-      <div>{{cache.data.levelsDom}}</div>
-      <div>{{cache.data.levelsWild}}</div>
-      <div>{{cache.data.mother}}</div>
-      <div>{{cache.data.mutations}}</div>
-      <div>{{cache.data.mutationsMaternal}}</div>
-      <div>{{cache.data.mutationsPaternal}}</div>
-      <div>{{cache.data.originServer}}</div>
-      <div>{{cache.data.owner}}</div>
-      <div>{{cache.data.speciesBP}}</div>
-      <div>{{cache.data.statValues}}</div>
-      <div>{{cache.data.status}}</div>
-      <div>{{cache.data.tags}}</div>
+      <div v-if="cache && cache.data">
+         <div>Name: {{cache.data.name}}</div>
+         <div>Species: {{cache.data.speciesName}}</div>
+         <div>Tribe: {{cache.data.tribe}}</div>
+         <div>{{cache.data.TE}}</div>
+         <div>{{cache.data.WL}}</div>
+         <div>{{cache.data.level}}</div>
+         <div>{{cache.data.isTamed}}</div>
+         <div>{{cache.data.isFemale}}</div>
+         <div>{{cache.data.inputSource}}</div>
+         <div>{{cache.data.father}}</div>
+         <div>{{cache.data.currentServer}}</div>
+         <div>{{cache.data.colors}}</div>
+         <canvas ref="creatureColors" width="150" height="150"></canvas>
+         <wheel></wheel>
+         <div>{{cache.data.breedingValues}}</div>
+         <div>{{cache.data.levelsDom}}</div>
+         <div>{{cache.data.levelsWild}}</div>
+         <div>{{cache.data.mother}}</div>
+         <div>{{cache.data.mutations}}</div>
+         <div>{{cache.data.mutationsMaternal}}</div>
+         <div>{{cache.data.mutationsPaternal}}</div>
+         <div>{{cache.data.originServer}}</div>
+         <div>{{cache.data.owner}}</div>
+         <div>{{cache.data.speciesBP}}</div>
+         <div>{{cache.data.statValues}}</div>
+         <div>{{cache.data.status}}</div>
+         <div>{{cache.data.tags}}</div>
+      </div>
    </b-container>
 </template>
 
@@ -42,24 +45,27 @@ import { Component } from 'vue-property-decorator';
 import Common from '../common';
 import { LiveDocument, CreateLiveDocument } from '@/data/firestore/live';
 import { Creature } from '@/data/firestore/objects';
+import { Delay } from '@/utils';
+import Wheel from '@/ui/components/wheel.vue';
 
-@Component({ name: 'Creature' })
+@Component({ name: 'Creature', components: { wheel: Wheel } })
 export default class extends Common {
    cache: LiveDocument<Creature> = null;
    colors: number[] = [];
    content: string = '';
 
-   mounted() {
+   async mounted() {
       this.cache = CreateLiveDocument<Creature>(`/library/${this.$route.params.library_id}/creature/${this.$route.params.creature_id}`);
-      while (!this.$refs.creatureColors);
+      await Delay(200);
       this.createCanvas();
    }
 
    beforeDestroy() {
       this.cache.close();
    }
+
    createCanvas() {
-      const c = this.$refs.creatureColors;
+      const c = this.$refs.creatureColors as HTMLCanvasElement;
       const ctx = c.getContext('2d');
       console.log(c);
       console.log(ctx);
