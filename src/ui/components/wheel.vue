cf2b87aa24a94b538e6f3aed8e964531a6dea4db
VolatilePulse <shearer.zachary@gmail.com>
2018-06-09 04:03:36 -0400

    Increase the appearance of displayed creatures

--- src/ui/components/wheel.vue
+++ src/ui/components/wheel.vue
@@ -1,16 +1,57 @@
 <template>
-   <svg></svg>
+   <svg :width="size" :height="size">
+      <path :id="index" :d="d" :transform="transform" :fill="colorCodes[regions[index]]" v-for="(transform, index) in transforms" :key="index">
+      </path>
+   </svg>
 </template>
 
 <style scoped>
 </style>
 
 <script lang="ts">
-import { Component } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import Common from '@/ui/common';
 
 @Component({ name: 'Wheel' })
 export default class Wheel extends Common {
-   // ...Vue code here
+   @Prop({ type: Number, default: 100 }) size: number;
+   @Prop({ type: Array }) colors: number[];
+
+   colorCodes = ['#808080', // Fallback Color
+      '#ff0000', '#0000ff', '#00ff00', '#ffff00', '#00ffff', '#ff00ff', '#c0ffba', '#c8caca',
+      '#786759', '#ffb46c', '#fffa8a', '#ff756c', '#7b7b7b', '#3b3b3b', '#593a2a', '#224900',
+      '#812118', '#ffffff', '#ffa8a8', '#592b2b', '#ffb694', '#88532f', '#cacaa0', '#94946c',
+      '#e0ffe0', '#799479', '#224122', '#d9e0ff', '#394263', '#e4d9ff', '#403459', '#ffe0ba',
+      '#948575', '#594e41', '#595959', '#ffffff', '#b79683', '#eadad5', '#d0a794', '#c3b39f',
+      '#887666', '#a0664b', '#cb7956', '#bc4f00', '#79846c', '#909c79', '#a5a48b', '#74939c',
+      '#787496', '#b0a2c0', '#6281a7', '#485c75', '#5fa4ea', '#4568d4', '#ededed', '#515151'
+   ];
+
+   // If no color regions, give it 1 default region
+   regions = (this.colors.length) ? this.colors.filter(num => num !== 0) : [0];
+
+   r = (this.size - 10) / 2;
+   degree = 360 / this.regions.length;
+   coords = this.coordinatesFromDegree(this.degree, this.r);
+   d = `M 0,0 l ${this.r},0 A ${this.r},${this.r},0,0,0,${this.coords.x},${-this.coords.y} L 0,0`;
+
+   transforms = this.generateRegions(this.degree, this.r, this.regions);
+
+   coordinatesFromDegree(degree: number, radius: number) {
+      // TODO: check if degree is valid
+      const angleInRadians = degree * (Math.PI / 180);
+      const y = Math.round(Math.sin(angleInRadians) * radius);
+      const x = Math.round(Math.cos(angleInRadians) * radius);
+      return { x, y };
+   }
+
+   generateRegions(degree: number, r: number, regions: number[]) {
+      const transforms: string[] = [];
+
+      for (let i = 0; i < regions.length; i++)
+         transforms.push(`rotate (${(i * degree) + degree / parseFloat('2')} ${r} ${r}) translate (${r}, ${r})`);
+
+      return transforms;
+   }
 }
 </script>
--- src/ui/views/creature.vue
+++ src/ui/views/creature.vue
@@ -3,50 +3,78 @@
       <h2>Creature</h2>
       <b-alert v-if="cache && cache.error" variant="danger">{{cache.error}}</b-alert>
       <b-btn :to="$route.path+'/edit'" variant="link">edit</b-btn>
-      <div v-if="cache && cache.data">
-         <div>Name: {{cache.data.name}}</div>
-         <div>Species: {{cache.data.speciesName}}</div>
-         <div>Tribe: {{cache.data.tribe}}</div>
-         <div>{{cache.data.TE}}</div>
-         <div>{{cache.data.WL}}</div>
-         <div>{{cache.data.level}}</div>
-         <div>{{cache.data.isTamed}}</div>
-         <div>{{cache.data.isFemale}}</div>
+      <!--
+         Offer different colored cards based on creature "mode"
+         <b-card v-if="cache && cache.data" v-bind:bg-variant="cache.data.isTamed ? 'success' : cache.data.isBred ? 'info ' : 'default '">
+      -->
+      <b-card v-if="cache && cache.data">
+         <h1>
+            <!-- Display Gender Symbol -->
+            <p v-if="cache.data.isFemale">&#9792;</p>
+            <p v-else>&#9794;</p>
+
+            <!-- If no creature name, display the species only -->
+            <!--
+               Display Species BP Path on hover
+               {{cache.data.speciesBP}}
+            -->
+            <p v-if="cache.data.name">{{cache.data.name}} ({{cache.data.speciesName}})</p>
+            <p v-else>{{cache.data.speciesName}}</p>
+         </h1>
+         <h3>LEVEL:
+            <p v-if="cache.data.level">{{cache.data.level}}</p>
+            <p v-else>{{calculatedLevel()}}</p>
+         </h3>
+         <h4>
+            <p v-if="cache.data.owner && cache.data.owner !== cache.data.tribe">{{cache.data.tribe}} - </p>
+            <p v-if="cache.data.tribe">{{cache.data.tribe}}</p>
+         </h4>
+         <!-- {{cache.data.colors}} -->
+         <wheel :size=200 :colors=cache.data.colors></wheel>
+         <div v-if="cache.data.isTamed">Tamed @ {{cache.data.WL}} w/ {{displayPercent(cache.data.TE)}} Taming Effectiveness</div>
+         <div v-else-if="cache.data.isBred">Bred @ {{cache.data.levelsWild[TORPOR] + 1}} w/ {{displayPercent(cache.data.IB)}} Imprinting</div>
+
+         <!--
+            Display Stat Values and Breeding Values
+            {{cache.data.breedingValues}}
+         -->
+         <b-row v-for="i in range(8)" :key=i>
+            <div>
+               <b-img :src="store.statImages[i]" fluid-grow class="align-items-center" style="max-height:28px;min-height:22px"></b-img>
+            </div>
+
+            <div>
+               {{cache.data.statValues}} = ({{cache.data.levelsWild[i]}} + {{cache.data.levelsDom[i]}})
+            </div>
+         </b-row>
+
+         <br />
          <div>{{cache.data.inputSource}}</div>
-         <div>{{cache.data.father}}</div>
+         <div>{{cache.data.originServer}}</div>
          <div>{{cache.data.currentServer}}</div>
-         <div>{{cache.data.colors}}</div>
-         <canvas ref="creatureColors" width="150" height="150"></canvas>
-         <wheel></wheel>
-         <div>{{cache.data.breedingValues}}</div>
-         <div>{{cache.data.levelsDom}}</div>
-         <div>{{cache.data.levelsWild}}</div>
-         <div>{{cache.data.mother}}</div>
+
          <div>{{cache.data.mutations}}</div>
          <div>{{cache.data.mutationsMaternal}}</div>
          <div>{{cache.data.mutationsPaternal}}</div>
-         <div>{{cache.data.originServer}}</div>
-         <div>{{cache.data.owner}}</div>
-         <div>{{cache.data.speciesBP}}</div>
-         <div>{{cache.data.statValues}}</div>
+         <div>{{cache.data.mother}}</div>
+         <div>{{cache.data.father}}</div>
+
          <div>{{cache.data.status}}</div>
          <div>{{cache.data.tags}}</div>
-      </div>
+      </b-card>
    </b-container>
 </template>
 
-
 <style lang="scss" scoped>
 </style>
-
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
 import Common from '../common';
 import { LiveDocument, CreateLiveDocument } from '@/data/firestore/live';
 import { Creature } from '@/data/firestore/objects';
-import { Delay } from '@/utils';
 import Wheel from '@/ui/components/wheel.vue';
+import { TORPOR } from '@/consts';
 
 @Component({ name: 'Creature', components: { wheel: Wheel } })
 export default class extends Common {
@@ -56,49 +84,22 @@ export default class extends Common {
 
    async mounted() {
       this.cache = CreateLiveDocument<Creature>(`/library/${this.$route.params.library_id}/creature/${this.$route.params.creature_id}`);
-      await Delay(200);
-      this.createCanvas();
    }
 
    beforeDestroy() {
       this.cache.close();
    }
+   displayPercent(num: number) {
+      return (num * 100).toFixed(2) + '%';
+   }
+   calculatedLevel() {
+      // All creatures start at level 1
+      let sum = 1 + this.cache.data.levelsWild[TORPOR];
 
-   createCanvas() {
-      const c = this.$refs.creatureColors as HTMLCanvasElement;
-      const ctx = c.getContext('2d');
-      console.log(c);
-      console.log(ctx);
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, 0, 1 / 3 * Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.lineWidth = 20;
-      ctx.fillStyle = '#FFFFFF';
-      ctx.fill();
-      ctx.strokeStyle = 'blue';
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, 1 / 3 * Math.PI, 2 / 3 * Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, 2 / 3 * Math.PI, Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, Math.PI, 4 / 3 * Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, 4 / 3 * Math.PI, 5 / 3 * Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.beginPath();
-      ctx.arc(100, 75, 50, 5 / 3 * Math.PI, 2 * Math.PI);
-      ctx.lineTo(100, 75);
-      ctx.closePath();
-      ctx.stroke();
+      for (const value of this.cache.data.levelsDom)
+         sum += value;
+
+      return sum;
    }
 }
 </script>
