9f285713d58bb31f7096f5fad311f372847b49aa
Ant Purchase <antoranthony.purchase@gmail.com>
2018-06-18 17:05:53 +0100

    Add missing components hooks for vue-router

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import '@/assets/scss/app.scss';
 import './registerServiceWorker';
 
 
+// PouchDB and its plugins
 import PouchDB from 'pouchdb-core';
 import PouchIDB from 'pouchdb-adapter-idb';
 import PouchFind from 'pouchdb-find';
@@ -13,7 +14,7 @@ PouchDB.plugin(PouchIDB);
 PouchDB.plugin(PouchFind);
 PouchDB.plugin(PouchLiveFind);
 
-
+// Vue and its plugins
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import PouchVue from 'pouch-vue';
@@ -21,6 +22,14 @@ import Router from 'vue-router';
 Vue.use(BootstrapVue);
 Vue.use(PouchVue, { pouch: PouchDB });
 Vue.use(Router);
+
+// Register Vue's router hooks with the class-component system
+import Component from 'vue-class-component';
+Component.registerHooks([
+   'beforeRouteEnter',
+   'beforeRouteLeave',
+   'beforeRouteUpdate' // for vue-router 2.2+
+]);
 
 
 import App from '@/ui/app.vue';
