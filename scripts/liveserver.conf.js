fdd344cde21254528ff292bf285b02ecc020c57b
Ant Purchase <antoranthony.purchase@gmail.com>
2018-06-24 15:47:47 +0100

    Allow testing of service worker with Firebase
    `npm run liveserver` now runs `firebase serve` and an https proxy so service workers can be fully tested.

--- scripts/liveserver.conf.js
+++ scripts/liveserver.conf.js
@@ -3,9 +3,12 @@ module.exports = {
    https: true,
    // http2: true,
    compress: true,
-   rewrite: [{ from: '/ASBMobile/*', to: '/$1' }],
+   rewrite: [
+      { from: '/__*', to: 'http://localhost:5000/__$1' },
+      { from: '/ASBMobile/*', to: '/$1' },
+   ],
    directory: 'docs',
    logFormat: 'dev', // 'stats' for non-scrolling status view
    key: '.user-certs/private-key.pem',
    cert: '.user-certs/asbm-cert.pem',
-}
+};
